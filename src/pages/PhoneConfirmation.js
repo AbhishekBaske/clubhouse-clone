0092231b5e2aa50de8e336b4062083fef2b5cd92
AbhishekBaske <a.baske86@gmail.com>
2023-06-14 05:23:13 +0000

    changes made

--- src/pages/PhoneConfirmation.js
+++ src/pages/PhoneConfirmation.js
@@ -7,7 +7,7 @@ export default function PhoneConfirmation() {
     const [value,setValue] = useState()
     return <div className={style.phoneConfirmationContainer}>
         <Link to="/" className={style.backBtn}>
-            <img src="../images/" alt="" />
+            <img src="../images/arrow.png" alt="" />
         </Link>
         <h1>Enter your phone</h1>
         <PhoneInput
@@ -17,5 +17,9 @@ export default function PhoneConfirmation() {
             onChange={setValue}
         />
         <p>By entering your number, You're agreeing to our <span>Terms of Services and Privacy Policy </span>Thanks!</p>
+        <Link to='/code_confirm'
+            className='primaryBtn d-flex align-items-center'>
+            Next <img src="../images/nextArrowIcon.svg" className="ml-1 "alt=""/>
+        </Link>
     </div>
 }
